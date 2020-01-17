8d9065d64c308704abb4769f4d934f6d4c12335a
WanmingZhang <claire.w.zhang@gmail.com>
2020-01-17 14:28:16 -0800

    Binary tree tilt.

--- Tree/BinaryTreeTilt.swift
+++ Tree/BinaryTreeTilt.swift
@@ -5,11 +5,13 @@
 //  Created by Zhang, Wanming - (p) on 7/26/18.
 //
 /**
-Given a binary tree, return the tilt of the whole tree.
+* Question link: https://leetcode.com/problems/binary-tree-tilt/
+ 
+* Given a binary tree, return the tilt of the whole tree.
 
-The tilt of a tree node is defined as the absolute difference between the sum of all left subtree node values and the sum of all right subtree node values. Null node has tilt 0.
+* The tilt of a tree node is defined as the absolute difference between the sum of all left subtree node values and the sum of all right subtree node values. Null node has tilt 0.
 
-The tilt of the whole tree is defined as the sum of all nodes' tilt.
+* The tilt of the whole tree is defined as the sum of all nodes' tilt.
 
  Input:
       1
@@ -24,30 +26,36 @@ The tilt of the whole tree is defined as the sum of all nodes' tilt.
 **/
 
 import Foundation
-class TreeNode {
-    var val: Int
-    var left: TreeNode?
-    var right: TreeNode?
-    init(_ value:Int) {
-        self.val = value
-        self.left = nil
-        self.right = nil
-    }
-}
+
+/**
+* Definition for a binary tree node.
+* public class TreeNode {
+*     public var val: Int
+*     public var left: TreeNode?
+*     public var right: TreeNode?
+*     public init(_ val: Int) {
+*         self.val = val
+*         self.left = nil
+*         self.right = nil
+*     }
+* }
+*/
+
 class Solution {
     var tilt = 0
     func findTilt(_ root: TreeNode?) -> Int {
-        treeSum(root)
+        treeSum(root: root)
         return tilt
     }
     
-    func treeSum(_ root: TreeNode?) -> Int {
-        if (root == nil) {
+    func treeSum(root: TreeNode?) -> Int {
+        guard let node = root else {
             return 0
         }
-        var leftSum = treeSum(root!.left)
-        var rightSum = treeSum(root!.right)
-        tilt += abs(leftSum - rightSum)
-        return leftSum + rightSum + root!.val
+        let leftSum = treeSum(root: node.left)
+        let rightSum = treeSum(root: node.right)
+        tilt = tilt + abs(leftSum - rightSum)
+        return leftSum + rightSum + node.val
     }
+    
 }
