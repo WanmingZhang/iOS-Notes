11e89c820256be53ae593ed033e1120a2895c042
WanmingZhang <claire.w.zhang@gmail.com>
2020-01-20 15:27:50 -0800

    SymmetricTree

--- Tree/SymmetricTree.swift
+++ Tree/SymmetricTree.swift
@@ -4,7 +4,8 @@
 //
 
 /**
-Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ * Problem link: https://leetcode.com/problems/symmetric-tree/
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
 
 For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
 
@@ -59,4 +60,54 @@ class Solution {
 }
 **/
 import Foundation
+
+
+// Definition for a binary tree node.
+//public class TreeNode {
+//
+//    public var val: Int
+//     public var left: TreeNode?
+//     public var right: TreeNode?
+//     public init(_ val: Int) {
+//         self.val = val
+//         self.left = nil
+//         self.right = nil
+//     }
+//
+//}
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+}
+
+class Solution {
+    public func isSymmetric(_ root: TreeNode?) -> Bool {
+        guard let root = root else {
+            return true
+        }
+        
+        return areSymmetricTrees(root.left, root2: root.right)
+    }
+    
+    func areSymmetricTrees(_ root1: TreeNode?, root2: TreeNode?) -> Bool {
+        if (root1 == nil && root2 == nil) {
+            return true
+        } else if (root1 == nil || root2 == nil) {
+            return false
+        } else if (root1!.val == root2!.val) {
+            return areSymmetricTrees(root1!.left, root2: root2!.right) &&
+                areSymmetricTrees(root1!.right, root2: root2!.left)
+        }
+        return false
+        
+    }
+}
+
 
